1bd80fab9d25f16ea3a6a78f1f43a7dca1d7c3f5
sam sumanth <samsumanth8991@gmail.com>
2021-04-23 10:43:27 +0530

    added: code

--- whileLoop/headOrTail-11.sh
+++ whileLoop/headOrTail-11.sh
@@ -1,16 +1,26 @@
-#!/bin/bash -x
-
-heads=1;
-tails=0;
-
-while [[ $heads -eq 11 && $tails -eq 11 ]]
+#! /bin/bash -x
+low=1
+upper=100
+chk=11
+countheads=0
+counttails=0
+while [ $low -lt $upper ]
 do
-Result=$((RANDOM%2))
-if [[ ${Result} -eq 0 ]]; then
-    echo HEADS
-elif [[ ${Result} -eq 1 ]]; then
-    echo TAILS
+random_no=$((RANDOM))
+val=$((random_no%2))
+if [ $val -eq 1 ]
+then
+echo "heads"
+countheads=$((countheads+1))
+else
+echo "tails"
+counttails=$((counttails+1))
 fi
+if [ $countheads -eq $chk -o $counttails -eq $chk ]
+then
+break
+fi
+low=$((low+1))
 done
-
+echo "Heads came"$countheads "times and tails came" $counttails "times"
 
