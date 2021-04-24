6dbaca1da87eff7cb5b351e7cf91394ef392776b
sam sumanth <samsumanth8991@gmail.com>
2021-04-24 17:07:27 +0530

    added: code

--- sequence-statement/sumAvgOfTwoDigitNum.sh
+++ sequence-statement/sumAvgOfTwoDigitNum.sh
@@ -1,7 +1,14 @@
-#!/bin/bash -x
-a=(((RANDOM%99( +1)))
-echo $a
-
+#! /bin/bash -x
+val1=$(( 10+RANDOM%89 ))
+val2=$(( 10+RANDOM%89 ))
+val3=$(( 10+RANDOM%89 ))
+val4=$(( 10+RANDOM%89 ))
+val5=$(( 10+RANDOM%89 ))
+echo "Five Random Numbers = "$val1 $val2 $val3 $val4 $val5
+sum=$(( $val1+$val2+$val3+$val4+$val5 ))
+avg=$(( $sum/5 ))
+echo "Sum = "$sum
+echo "Average = "$avg
 
 
 
